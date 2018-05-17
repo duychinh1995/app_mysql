720b2388269c3159b5c2a21ae96353421a05bde7
Hoang Duy Chinh <hduychinh@gmail.com>
2018-05-17 22:38:17 +0700

    addd

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20180517142137) do
 
-  create_table "meos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "meos", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
